f834aec19eb107d0ab901b41e27b8db2fa62ec64
Bimo Alfarrabi <bimoalfarrabi24@gmail.com>
2022-10-06 07:11:17 +0700

    Status: success: course 4.1.3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +25,14 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('admin.index');
     })->name('dashboard');
+});
+
+Route::get('/admin/logout',[AdminController::class, 'logout'])->name('admin.logout');
+
+//semua route untuk user
+
+Route::prefix('users')->group(function(){
+    Route::get('/view',[UserController::class, 'UserView'])->name('user.view');
 });
