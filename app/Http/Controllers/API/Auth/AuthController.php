76404febf253c740a017ad805a86909fda1e261a
bimoalfarrabi <46051173+bimoalfarrabi@users.noreply.github.com>
2022-12-09 09:58:49 +0700

    5.1.3 API auth login

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 
 class AuthController extends Controller
@@ -27,5 +28,35 @@ class AuthController extends Controller
         $user->save;
         
         return response()->json($user, 201);
+    }
+
+    public function login(Request $request){
+        $validateData = $request->validate([
+            'email' => 'email | required | unique:users',
+            'password' => 'required | confirmed'
+        ]);
+
+        $login_detail = request(['email','password']);
+
+        if(!Auth::attempt($login_detail)){
+            return response()->json([
+                'error' => 'Login gagal. Cek detail login kembali'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('AccessToken');
+        $token = $tokenResult->token;
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_id' => $token->id,
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email
+        ], 200);
+
     }
 }
